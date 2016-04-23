cd494ad16ec72505a273ceb06ed38113d7cf662e
spacegeek224 <marisusisme@gmail.com>
2016-04-23 17:10:18 +0000

    num words

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-module.exports = {
+module.exports = {};
+
+module.exports.word = {
   isWord: function(s) {
     return (String(s).match(/(\w+)\S/g).length == 1);
+  },
+  howManyWords: function(s) {
+    return String(s).match(/(\w+)\S/g).length;
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,18 @@
 var should = require('chai').should(),
-  wording = require('../index'),
-  isWord = wording.isWord;
+  wording = require('../index');
 
 describe('#isWord', function() {
   it('returns false if input is > 1 word', function() {
-    isWord('hello world').should.equal(false);
+    wording.word.isWord('hello world').should.equal(false);
   });
   it('returns true if input is 1 word', function() {
-    isWord('hello').should.equal(true);
+    wording.word.isWord('hello').should.equal(true);
   });
 
+});
+
+describe('#howManyWords', function() {
+  it('returns number of words',function() {
+    wording.word.howManyWords('hello world').should.equal(2);
+  });
 });
